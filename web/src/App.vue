9893c4df9503b07eeecddd75e52b0af6fa6b0c68
yanhe6352 <951074003@qq.com>
2019-06-17 17:17:37 +0800

    创建了购物车

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,36 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
+    <div id="app">
+        <MyShoppingCart></MyShoppingCart>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyShoppingCart from './components/ShoppingCart.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'app',
+    components: {
+        MyShoppingCart
+    }
+};
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
 }
 </style>
